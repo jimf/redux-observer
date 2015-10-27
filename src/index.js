3767c67c764ebf77d945aab3ba4bf087a0206b4e
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2015-10-27 08:32:54 -0400

    Add jsdoc docstring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const isStrictlyEqual = (obj1, obj2) => obj1 === obj2;
 
+/**
+ * Observer middleware factory.
+ *
+ * @param {Function} onUpdate Callback to trigger when state is changed.
+ * @param {Object} [options] Configuration options
+ * @param {Function} [options.compareWith] Predicate function for comparison
+ * @returns {Function}
+ */
 export default function observerMiddleware(onUpdate, options = {}) {
   const pred = options.compareWith || isStrictlyEqual;
 
